55c8fb9e1a2693e8ce41a1227f1b020a47b1ae26
seyedrezafar <seyedrezafar1093@gmail.com>
2021-03-03 14:53:25 +0330

    import django rest filters

--- apps/packages/api/views.py
+++ apps/packages/api/views.py
@@ -1,5 +1,5 @@
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import viewsets, status
+from rest_framework import viewsets, status, filters
 from rest_framework.mixins import ListModelMixin, RetrieveModelMixin
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
