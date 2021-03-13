0d1dff059d4b261f22d3acdac2e042885c29a044
Arash F <arash@hami.blue>
2021-03-13 13:13:27 +0330

    Added packages to category serializer

--- apps/packages/api/serializers.py
+++ apps/packages/api/serializers.py
@@ -3,8 +3,16 @@ from rest_framework import serializers
 from apps.packages.models import Package, PackageCategory
 
 
+class PackageSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Package
+        fields = ('id', 'name', 'price')
+
+
 class PackageCategorySerializer(serializers.ModelSerializer):
     children = serializers.SerializerMethodField()
+    packages = serializers.SerializerMethodField()
 
     class Meta:
         model = PackageCategory
@@ -12,12 +20,6 @@ class PackageCategorySerializer(serializers.ModelSerializer):
 
     def get_children(self, obj):
         return PackageCategorySerializer(obj.children.filter(is_enable=True), many=True).data
-
-
-class PackageSerializer(serializers.ModelSerializer):
-    category = PackageCategorySerializer()
-
-    class Meta:
-        model = Package
-        fields = ['id', 'name', 'category', 'price', 'discount', ]
-
+    
+    def get_packages(self, obj):
+        return PackageSerializer(Package.objects.filter(category=obj, is_enable=True), many=True).data
