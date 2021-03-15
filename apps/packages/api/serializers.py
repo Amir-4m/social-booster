6cec3ffc1e301e88db2a59a916e0d838d128e8fe
Arash F <arash@hami.blue>
2021-03-15 17:31:54 +0330

    absolute url for children

--- apps/packages/api/serializers.py
+++ apps/packages/api/serializers.py
@@ -19,7 +19,7 @@ class PackageCategorySerializer(serializers.ModelSerializer):
         fields = ('id', 'title', 'icon', 'slug', 'is_dynamic_price', 'children', 'packages', 'input_label', )
 
     def get_children(self, obj):
-        return PackageCategorySerializer(obj.children.filter(is_enable=True), many=True).data
+        return PackageCategorySerializer(obj.children.filter(is_enable=True), many=True, context=self.context).data
     
     def get_packages(self, obj):
         return PackageSerializer(Package.objects.filter(category=obj, is_enable=True), many=True).data
