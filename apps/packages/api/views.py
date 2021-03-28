ec30002c45ab5f6d5035fc762e75ec57de4c6f9b
seyedrezafar <seyedrezafar1093@gmail.com>
2021-03-28 08:39:15 +0430

    Make code more robust in the case that interval does not exist

--- apps/packages/api/views.py
+++ apps/packages/api/views.py
@@ -1,9 +1,10 @@
 from django.db.models import Q
 from rest_framework.decorators import action
+from rest_framework.exceptions import ValidationError
 from rest_framework.mixins import ListModelMixin, RetrieveModelMixin
 from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
-
+from django.utils.translation import ugettext_lazy as _
 from apps.packages.api.serializers import PackageCategorySerializer, PackageSerializer
 from apps.packages.models import PackageCategory
 from utils.utils import telegram_member_count
@@ -40,14 +41,18 @@ class PackageCategoryViewSet(ListModelMixin,
 
         for package in serializer_data['packages']:
             members_count = telegram_member_count(request.GET.get('link'))
-            suitable_interval_price = list(filter(lambda x: x['amount_interval'].lower <= members_count <= x['amount_interval'].upper,
-                                                  price_intervals))[0]['price_per_interval']
-            package['price'] = suitable_interval_price * members_count
-            if package['discount'] != 0:
-                # the package final_price should be computed here if the discount is not 0
-                package['final_price'] = int(package['price'] - (package['price'] * (package['discount']/100)))
+            valid_interval_list = list(filter(lambda x: x['amount_interval'].lower <= members_count <= x['amount_interval'].upper,
+                                              price_intervals))
+            if len(valid_interval_list) > 0:
+                suitable_interval_price = valid_interval_list[0]['price_per_interval']
+                package['price'] = suitable_interval_price * members_count
+                if package['discount'] != 0:
+                    # the package final_price should be computed here if the discount is not 0
+                    package['final_price'] = int(package['price'] - (package['price'] * (package['discount']/100)))
+                else:
+                    package['final_price'] = package['price']
             else:
-                package['final_price'] = package['price']
+                raise ValidationError(_("There is no interval for the given value"))
 
         return Response({'test': serializer_data})
 
