b4f1ad545e2bf91fb428a5af6df984e879bca54b
Arash F <arash@hami.blue>
2021-04-10 11:30:02 +0430

    logger for payment_request

--- apps/payments/services.py
+++ apps/payments/services.py
@@ -1,5 +1,9 @@
+import logging
 import requests
+
 from django.conf import settings
+
+logger = logging.getLogger(__name__)
 
 
 class CustomService(object):
@@ -15,6 +19,17 @@ class CustomService(object):
             'post': requests.post
         }
 
-        _r = methods[method](f"{settings.PAYMENT_API_URL}{endpoint}/", headers=headers, json=data, timeout=(3.05, 30))
-        _r.raise_for_status()
+        try:
+            _r = methods[method](f"{settings.PAYMENT_API_URL}{endpoint}/", headers=headers, json=data, timeout=(3.05, 30))
+            _r.raise_for_status()
+        except requests.exceptions.HTTPError as e:
+            logger.warning(f'[payment gateway]-[HTTPError]-[endpoint: {endpoint}]-[status code: {e.response.status_code}]-[err: {e.response.text}]')
+            raise
+        except requests.exceptions.ConnectTimeout as e:
+            logger.critical(f'[payment gateway]-[ConnectTimeout]-[endpoint: {endpoint}]-[err: {e}]')
+            raise
+        except Exception as e:
+            logger.error(f'[payment gateway]-[{type(e)}]-[endpoint: {endpoint}]-[err: {e}]')
+            raise
+
         return _r.json()
